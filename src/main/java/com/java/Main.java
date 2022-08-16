c1c658036b3511f8e99fd55570fb51de62ab1ca2
Valeriia Diachenko <Valerusya55@mail.ru>
2022-08-16 17:25:20 +0600

    renue

--- src/main/java/com/java/Main.java
+++ src/main/java/com/java/Main.java
@@ -7,16 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String data = null;
+        System.out.println("Введите строку для поиска:");
+        String data = in.nextLine();
         String line;
         String searchLine;
-        int count;
         Map<String, String> result = new TreeMap<>();
-        while (!Objects.equals(data, "!quit")) {
+        while (!data.equals("!quit")) {
             result.clear();
-            count = 0;
-            System.out.println("Введите строку для поиска:");
-            data = in.nextLine();
             try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/src/airports.csv"))) {
                 long m = System.currentTimeMillis();
                 while ((line = reader.readLine()) != null) {
@@ -24,21 +21,19 @@ public class Main {
                     searchLine = cols[Integer.parseInt(args[0].trim()) - 1].replaceAll("^\"|\"$", "");
                     if (searchLine.regionMatches(true, 0, data, 0, data.length())) {
                         result.put(searchLine, line);
-                        count++;
                     }
                 }
                 for (Map.Entry<String, String> pair : result.entrySet()) {
                     System.out.println(pair.getKey() + "[" + pair.getValue() + "]");
                 }
-                System.out.println("Строк:" + count);
+                System.out.println("Строк:" + result.size());
                 System.out.println("Время поиска:" + (double) (System.currentTimeMillis() - m) + " мс");
-            } catch (ArrayIndexOutOfBoundsException | FileNotFoundException | NumberFormatException e) {
-                if (e.getClass() == ArrayIndexOutOfBoundsException.class || e.getClass() == NumberFormatException.class) {
-                    System.out.println("Не правильно задан параметр");
-                } else {
-                    System.out.println("Файл не найден");
-                }
-                break;
+                System.out.println("Введите строку для поиска:");
+                data = in.nextLine();
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                System.out.println("Не правильно задан параметр");
+            } catch (FileNotFoundException e) {
+                System.out.println("Файл не найден");
             } catch (IOException e) {
                 e.printStackTrace();
             }
